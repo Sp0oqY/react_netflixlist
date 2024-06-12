2311053e4636413e33608be4bb868bcc1334f1f2
Sp0oqY <kastierm45@gmail.com>
2024-06-13 00:31:22 +0200

    I created router and core for pages

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HeaderFooter.css'
+
+export default function Header() {
+    return (
+        <header>
+          <ul className="nav">
+            <li><a href="https://main.drzyxmq7a3ydx.amplifyapp.com">Home</a></li>
+            <li><a href="https://main.drzyxmq7a3ydx.amplifyapp.com">Exercises</a></li>
+            <li><a href="https://main.drzyxmq7a3ydx.amplifyapp.com">Meals</a></li>
+            <li><a href="https://main.drzyxmq7a3ydx.amplifyapp.com">Supplements</a></li>
+            <li><a href="https://main.drzyxmq7a3ydx.amplifyapp.com/contact">Contact</a></li>
+          </ul>
+        </header>
+    )
+}
