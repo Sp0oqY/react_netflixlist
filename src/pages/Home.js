319aae7086205eb267e077dc4b6a7728affc706f
Sp0oqY <kastierm45@gmail.com>
2024-06-16 18:19:54 +0200

    I am just doing home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,18 @@ export default function Home() {
     return (
         <div>
             <h1> Welcome to the home page of Netflix list :)</h1>
+
+            <div class="content-news">
+                <h1>News</h1>
+                <h4>New 1</h4>
+                <img id="imgLeft" src="" alt="" />
+                <p>Description</p>
+                <small>Date</small>
+
+                <section class="page">
+                    <a href="/rocnikovy?page=<?= $i ?>">1</a>
+                </section>
+            </div>
         </div>
     );
 }
